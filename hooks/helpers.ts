cd7048cc97553010e08c07ebb7fae9cbd1b35a77
Rosamund Mather <rsmnd.mthr@gmail.com>
2025-06-09 22:36:16 +0200

    Restructure static page routes

--- hooks/helpers.ts
+++ hooks/helpers.ts
@@ -5,12 +5,4 @@ export const getMarker = (street: Street) => {
     let marker = greenMarker;
     if (!street.imagePath) marker = redMarker
     return marker
-  }
-
-export const getImpressumAddress = (): string[] | undefined => {
-    if (process.env.IMPRESSUM_ADDRESS) {
-        return process.env.IMPRESSUM_ADDRESS.split(",")
-    }
 }
-
-export const getImpressumEmail = (): string | undefined => process.env.IMPRESSUM_EMAIL
