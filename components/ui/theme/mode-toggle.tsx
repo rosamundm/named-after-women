1f4f47baefba6c5efbc56bc4d62f6263aa1756d6
Rosamund Mather <rsmnd.mthr@gmail.com>
2025-09-27 20:29:14 +0200

    Fix formatting

--- components/ui/theme/mode-toggle.tsx
+++ components/ui/theme/mode-toggle.tsx
@@ -19,19 +19,15 @@ const ModeToggle: FC = () => {
 	<DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun
-		  	className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"
-		  />
-          <Moon
-		  	className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
-		  />
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">
-			Toggle theme
-		  </span>
+			      Toggle theme
+		      </span>
         </Button>
       </DropdownMenuTrigger>
 	  <DropdownMenuContent asChild align="end">
-      <div className="rounded-lg shadow-md bg-white dark:bg-neutral-900">
+      <div className="rounded-lg shadow-md bg-neutral-900 light:bg-white">
 	  	  {['light', 'dark', 'system'].map((mode) => (
   			  <DropdownMenuItem key={mode} className="p-2" onClick={() => setTheme(mode)}>
     		    {mode}
